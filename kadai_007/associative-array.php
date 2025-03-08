b305056074dc4753629da5dfddc76edbb3453c38
yuri0422 <mfam062220@gmail.com>
2025-03-09 02:19:50 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入
+    $vegetables = ['name' => 'onion','price' => 200,'weight' => 160];
+
+    // 値を出力
+    print_r($vegetables);
+    ?>
+  </p>
+<body>
+
+</html>
